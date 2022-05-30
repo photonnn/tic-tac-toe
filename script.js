fe0308390b4547cbd5b88b2ad9eed9c0440c503a
photonnn <wasjiri@gmail.com>
2022-05-30 14:36:15 +0200

    Add event listeners for boxes, change font

--- script.js
+++ script.js
@@ -25,4 +25,14 @@
 
     console.log(gameboard.array);
 
+    const boxes = [...document.querySelectorAll(".box")];
+    for (let em of boxes) {
+        if (!em.classList.contains("taken")) {
+            em.addEventListener('click', () => {
+                em.classList.add("taken");
+                em.innerHTML = "X";
+            });
+        }
+    }
+
 })();
